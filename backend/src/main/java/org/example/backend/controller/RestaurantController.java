04d665802e7fadaf434a602260f941ae7b3ac809
BaselQq <73541125+BaselQq@users.noreply.github.com>
2024-04-30 11:27:41 +0200

    add rud endpoints

--- backend/src/main/java/org/example/backend/controller/RestaurantController.java
+++ backend/src/main/java/org/example/backend/controller/RestaurantController.java
@@ -1,8 +1,12 @@
 package org.example.backend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.backend.dto.DetailImagesUrls;
+import org.example.backend.dto.Favourite;
+import org.example.backend.dto.HeroImage;
 import org.example.backend.dto.RestaurantRecord;
 import org.example.backend.model.Restaurant;
+import org.example.backend.repository.RestaurantRepository;
 import org.example.backend.service.RestaurantService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +22,7 @@ public class RestaurantController {
 
 
     private final RestaurantService restaurantService;
+    private final RestaurantRepository repo;
 
 
     @GetMapping("/api/restaurants")
@@ -39,10 +44,73 @@ public class RestaurantController {
         return restaurantService.getAllByType(type);
     }
 
-    @GetMapping("/api/favourit/restaurants")
+    @GetMapping("/api/favourite/restaurants")
     public List<Restaurant> getAllRestaurantByFavouriteIsTrue () {
 
         return restaurantService.getAllFavourite();
+    }
+
+    @PutMapping("/api/updaterestaurantname/{id}/{name}")
+    void updateRestaurantByName(@PathVariable String id, @PathVariable String name) {
+        Restaurant restaurant = repo.findById(id).orElse(null);
+
+        repo.save(restaurant.withName(name));
+    }
+
+    @PutMapping("/api/updaterestaurantdescription/{id}/{description}")
+    void updateRestaurantByDescription(@PathVariable String id, @PathVariable String description) {
+        Restaurant restaurant = repo.findById(id).orElse(null);
+
+        repo.save(restaurant.withDescription(description));
+    }
+
+    @PutMapping(path = {"/api/updaterestaurantfavourite/{id}", "{favourite}"})
+    void updateRestaurantByFavourite(@PathVariable String id, @RequestBody Favourite favourite) {
+        Restaurant restaurant = repo.findById(id).orElse(null);
+
+        repo.save(restaurant.withFavourite(favourite.isFavourite()));
+    }
+
+    @PutMapping("/api/updaterestaurantrating/{id}/{rating}")
+    void updateRestaurantByDescription(@PathVariable String id, @PathVariable int rating) {
+        Restaurant restaurant = repo.findById(id).orElse(null);
+
+        repo.save(restaurant.withRating(rating));
+    }
+
+    @PutMapping("/api/updaterestauranttype/{id}/{type}")
+    void updateRestaurantByType(@PathVariable String id, @PathVariable String type) {
+        Restaurant restaurant = repo.findById(id).orElse(null);
+
+        repo.save(restaurant.withType(type));
+    }
+
+    @PutMapping("/api/updaterestaurantlocation/{id}/{location}")
+    void updateRestaurantByLocation(@PathVariable String id, @PathVariable String location) {
+        Restaurant restaurant = repo.findById(id).orElse(null);
+
+        repo.save(restaurant.withLocation(location));
+    }
+
+    @PutMapping({"/api/updaterestaurantheroimage/{id}", "{heroImage}"})
+    void updateRestaurantByHeroImage(@PathVariable String id, @RequestBody HeroImage heroImage) {
+        Restaurant restaurant = repo.findById(id).orElse(null);
+
+        repo.save(restaurant.withHeroImage(heroImage.heroImage()));
+    }
+
+    @PutMapping({"/api/updaterestaurantdetailimageurls/{id}", "{detailImageUrls}"})
+    void updateRestaurantByDetailImageUrls(@PathVariable String id, @RequestBody DetailImagesUrls detailImageUrls) {
+        Restaurant restaurant = repo.findById(id).orElse(null);
+
+        repo.save(restaurant.withDetailImagesUrls(detailImageUrls.detailImageUrls()));
+    }
+
+    @DeleteMapping("/api/removerestaurant/{id}")
+    public void removeRestaurantById(@PathVariable String id) {
+        Restaurant restaurant = repo.findById(id).orElse(null);
+
+        repo.delete(restaurant);
     }
 
     @PostMapping("/api/newrestaurant")
